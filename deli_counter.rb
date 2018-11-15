41da085d9546a0cff7dcfa0685c66efefad31f6e
Shaundra <s.m.crittenden@gmail.com>
2018-11-15 03:00:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,8 +26,7 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    line_state = "The line is currently: "
-    line_state << array.map.with_index { |name, idx| "#{idx + 1}. #{name}"}.join(" ")
-    puts line_state
+    line_state = array.map.with_index { |name, idx| "#{idx + 1}. #{name}"}.join(" ")
+    puts "The line is currently: #{line_state}"
   end
 end
